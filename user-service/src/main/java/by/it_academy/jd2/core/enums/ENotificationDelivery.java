8cc619d37701ba8d643e0d3022b19804a7f1e0f1
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-01 22:00:22 +0300

    add Enums classes

--- user-service/src/main/java/by/it_academy/jd2/core/enums/ENotificationDelivery.java
+++ user-service/src/main/java/by/it_academy/jd2/core/enums/ENotificationDelivery.java
@@ -1,4 +1,4 @@
-package by.it_academy.jd2.Mk_JD2_98_23.core.enums;
+package by.it_academy.jd2.core.enums;
 
 public enum ENotificationDelivery {
 
---
+++ user-service/src/main/java/by/it_academy/jd2/core/enums/ERole.java
@@ -0,0 +1,9 @@
+package by.it_academy.jd2.core.enums;
+
+public enum ERole {
+
+    ADMIN,
+
+    USER,
+
+}
