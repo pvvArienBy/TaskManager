9b13e7265a854463281721facba46257a9f3dffc
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-02 00:49:52 +0300

    ref IUserService.java
    remove method

--- user-service/src/main/java/by/it_academy/jd2/service/api/IUserService.java
+++ user-service/src/main/java/by/it_academy/jd2/service/api/IUserService.java
@@ -6,7 +6,5 @@ import by.it_academy.jd2.dao.entity.UserEntity;
 
 public interface IUserService extends ICRUDService <UserEntity, UserCreateDTO, UserDTO>{
 
-    boolean checkChildren (Long id);
-
     boolean validateCoordinatesParam(String id);
 }
