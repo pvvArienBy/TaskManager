1ff519bb9d0435ed166faf205e90ef54563d445d
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-04 21:24:53 +0300

    ref UserServlet.java
    add method get and post + test
    
    no test filter

--- user-service/src/main/java/by/it_academy/jd2/controllers/endpoints/web/UserServlet.java
+++ user-service/src/main/java/by/it_academy/jd2/controllers/endpoints/web/UserServlet.java
@@ -2,19 +2,34 @@ package by.it_academy.jd2.controllers.endpoints.web;
 
 import by.it_academy.jd2.controllers.endpoints.web.ac.api.IAppContextAnnotation;
 import by.it_academy.jd2.controllers.endpoints.web.ac.factory.AppContextAnnotationFactory;
+import by.it_academy.jd2.core.dto.UserCreateDTO;
+import by.it_academy.jd2.core.dto.UserDTO;
+import by.it_academy.jd2.dao.entity.UserEntity;
 import by.it_academy.jd2.service.api.IUserService;
+import by.it_academy.jd2.service.util.UserConvertUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(urlPatterns = "/api/user")
 public class UserServlet extends HttpServlet {
 
-    private IUserService userService;
+    private static final String USER_ID = "id";
+    private static final String USER_VERSION = "version";
+    private static final String TEXT_FILTER = "filter";
 
-    private ObjectMapper objectMapper;
+    private static  IUserService userService;
 
-    public UserServlet() {
-    }
+    private static ObjectMapper objectMapper;
 
     @Override
     public void init() throws ServletException {
@@ -22,5 +37,101 @@ public class UserServlet extends HttpServlet {
         IAppContextAnnotation appContextAnnotation = AppContextAnnotationFactory.getInstance();
         this.userService = appContextAnnotation.getUserService();
         this.objectMapper =  appContextAnnotation.getObjectMapper();
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+
+        String idSt = req.getParameter(USER_ID);
+        String filter = req.getParameter(TEXT_FILTER);
+
+        if (userService.validateCoordinatesParam(idSt) && filter == null) {
+            Long id = Long.parseLong(idSt);
+            UserDTO dto = UserConvertUtil.toDTO(userService.get(id));
+
+            resp.setStatus(HttpServletResponse.SC_OK);
+            writer.write(objectMapper.writeValueAsString(dto));
+
+        } else if (filter != null && idSt == null) {
+            List<UserDTO> departmentDTOS = new ArrayList<>();
+
+            for (UserEntity entity : userService.filterByName(filter)) {
+                departmentDTOS.add(UserConvertUtil.toDTO(entity));
+            }
+
+            resp.setStatus(HttpServletResponse.SC_OK);
+            writer.write(objectMapper.writeValueAsString(departmentDTOS));
+
+        } else {
+            List<UserDTO> departmentDTOS = new ArrayList<>();
+
+            for (UserEntity entity : userService.get()) {
+                departmentDTOS.add(UserConvertUtil.toDTO(entity));
+            }
+            resp.setStatus(HttpServletResponse.SC_OK);
+            writer.write(objectMapper.writeValueAsString(departmentDTOS));
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+        InputStream requestBody = req.getInputStream();
+
+        UserCreateDTO departmentCreateDTO = objectMapper.readValue(requestBody, UserCreateDTO.class);
+        UserDTO departmentDTO = UserConvertUtil.toDTO(userService.add(departmentCreateDTO));
+
+        resp.setStatus(HttpServletResponse.SC_CREATED);
+        writer.write(objectMapper.writeValueAsString(departmentDTO) + "- ADD DEPARTMENT");
+    }
+
+
+//    @Override
+//    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+//        PrintWriter writer = resp.getWriter();
+//        String idStr = req.getParameter(USER_ID);
+//        String versionStr = req.getParameter(USER_VERSION);
+//        InputStream requestBody = req.getInputStream();
+//
+//        DepartmentCreateUpdateDTO departmentDTO;
+//
+//        try {
+//            departmentDTO = objectMapper.readValue(requestBody, DepartmentCreateUpdateDTO.class);
+//        } catch (IOException e) {
+//            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+//            writer.write("Проверьте данные, возможно вы не указали родителя!");
+//            return;
+//        }
+//        if (userService.validateCoordinatesParam(idStr) && userService.validateCoordinatesParam(versionStr)) {
+//            Long id = Long.parseLong(idStr);
+//            Long version = Long.parseLong(versionStr);
+//            CoordinatesDTO depCoordinatesDTO = new CoordinatesDTO(id,version);
+//
+//
+//            userService.update(depCoordinatesDTO, departmentDTO);
+//
+//            resp.setStatus(HttpServletResponse.SC_OK);
+//            UserDTO newDTO = UserConvertUtil.toDTO(userService.get(id));
+//
+//            writer.write(objectMapper.writeValueAsString(newDTO) + " - UPDATE DEPARTMENT");
+//        }
+//    }
+
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+        String idStr = req.getParameter(USER_ID);
+
+        if (userService.validateCoordinatesParam(idStr)) {
+            Long id = Long.parseLong(idStr);
+
+                userService.remove(id);
+
+                resp.setStatus(HttpServletResponse.SC_OK);
+                writer.write("Департамент удалён");
+        } else {
+            writer.write("Введите корректные данные");
+        }
     }
 }
