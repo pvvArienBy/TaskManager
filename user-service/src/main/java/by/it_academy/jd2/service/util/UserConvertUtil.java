fdfa5d853ad22cbfceef4ef64dcfb899fdeb7528
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-04 21:22:20 +0300

    ref UserConvertUtil.java
    switch old realization

--- user-service/src/main/java/by/it_academy/jd2/service/util/UserConvertUtil.java
+++ user-service/src/main/java/by/it_academy/jd2/service/util/UserConvertUtil.java
@@ -3,7 +3,6 @@ package by.it_academy.jd2.service.util;
 import by.it_academy.jd2.core.dto.UserCreateDTO;
 import by.it_academy.jd2.core.dto.UserDTO;
 import by.it_academy.jd2.dao.entity.UserEntity;
-import org.springframework.beans.BeanUtils;
 
 import java.util.Objects;
 
@@ -14,12 +13,18 @@ public class UserConvertUtil  {
      *
      * @param item the UserEntity object to convert
      * @return the converted UserDTO object
-     * todo need test and debug test
      */
     public static UserDTO toDTO(UserEntity item) {
         Objects.requireNonNull(item, "The item parameter cannot be null");
         UserDTO dto = new UserDTO();
-        BeanUtils.copyProperties(dto, item);
+        dto.setId(item.getId());
+        dto.setFirstName(item.getFirstName());
+        dto.setLastName(item.getLastName());
+        dto.setMail(item.getMail());
+        dto.setTelegram(item.getTelegram());
+        dto.setPosition(item.getPosition());
+        dto.setRole(item.getRole());
+        dto.setStatus(item.getStatus());
         return dto;
     }
 
@@ -28,41 +33,18 @@ public class UserConvertUtil  {
      *
      * @param dto the UserDTO object to convert
      * @return the converted UserEntity object
-     * todo need test and debug test
      */
     public static UserEntity toEntity(UserCreateDTO dto) {
         Objects.requireNonNull(dto, "The dto parameter cannot be null");
         UserEntity entity = new UserEntity();
-        BeanUtils.copyProperties(entity, dto);
+        entity.setFirstName(dto.getFirstName());
+        entity.setLastName(dto.getFirstName());
+        entity.setMail(dto.getMail());
+        entity.setPassword(dto.getPassword());
+        entity.setTelegram(dto.getTelegram());
+        entity.setPosition(dto.getPosition());
+        entity.setRole(dto.getRole());
+        entity.setStatus(dto.getStatus());
         return entity;
     }
 }
-
-
-//todo check after
-
-//    public static UserDTO toDTO(UserEntity item) {
-//        UserDTO dto = new UserDTO();
-//        dto.setId(item.getId());
-//        dto.setFirstName(item.getFirstName());
-//        dto.setLastName(item.getLastName());
-//        dto.setMail(item.getMail());
-//        dto.setTelegram(item.getTelegram());
-//        dto.setPosition(item.getPosition());
-//        dto.setRole(item.getRole());
-//        dto.setStatus(item.getStatus());
-//        return dto;
-//    }
-
-//    private UserEntity toEntity(UserCreateDTO item) {
-//        return new UserEntity(
-//                item.getFirstName(),
-//                item.getLastName(),
-//                item.getMail(),
-//                item.getPassword(),
-//                item.getTelegram(),
-//                item.getPosition(),
-//                item.getRole(),
-//                item.getStatus(),
-//                item.getNotificationWay());
-//    }
