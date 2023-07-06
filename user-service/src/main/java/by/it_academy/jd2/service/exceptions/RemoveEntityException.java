55371c6fc26d884496be132e7f027db28690f70f
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-06 10:34:47 +0300

    add RemoveEntityException.java     UnknownDepFormatException.java     UpdateEntityException.java

---
+++ user-service/src/main/java/by/it_academy/jd2/service/exceptions/RemoveEntityException.java
@@ -0,0 +1,25 @@
+package by.it_academy.jd2.service.exceptions;
+
+
+
+public class RemoveEntityException extends RuntimeException{
+    public RemoveEntityException() {
+        super();
+    }
+
+    public RemoveEntityException(String message) {
+        super(message);
+    }
+
+    public RemoveEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RemoveEntityException(Throwable cause) {
+        super(cause);
+    }
+
+    protected RemoveEntityException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
