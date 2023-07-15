630323e1f7e39e1dc0e92de22e73d3371d3e6124
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-15 21:05:29 +0300

    ref IProjectService.java     ProjectService.java

---
+++ task-service/src/main/java/by/it_academy/jd2/service/ProjectService.java
@@ -0,0 +1,55 @@
+package by.it_academy.jd2.service;
+
+import by.it_academy.jd2.core.dto.CoordinatesDTO;
+import by.it_academy.jd2.core.dto.ProjectCreateDTO;
+import by.it_academy.jd2.dao.api.IProjectDao;
+import by.it_academy.jd2.dao.entity.ProjectEntity;
+import by.it_academy.jd2.service.api.IProjectService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectService implements IProjectService {
+
+    private final IProjectDao projectDao;
+
+    public ProjectService(IProjectDao projectDao) {
+        this.projectDao = projectDao;
+    }
+
+    @Override
+    public List<ProjectEntity> get() {
+        return null;
+    }
+
+    @Override
+    public ProjectEntity get(Long id) {
+        return null;
+    }
+
+    @Override
+    public ProjectEntity add(ProjectCreateDTO item) {
+        return null;
+    }
+
+    @Override
+    public void remove(CoordinatesDTO coordinates) {
+
+    }
+
+    @Override
+    public void update(CoordinatesDTO coordinates, ProjectCreateDTO item) {
+
+    }
+
+    @Override
+    public List<ProjectEntity> filterByName(String text) {
+        return null;
+    }
+
+    @Override
+    public boolean validateCoordinatesParam(String id) {
+        return false;
+    }
+}
---
+++ task-service/src/main/java/by/it_academy/jd2/service/api/IProjectService.java
@@ -0,0 +1,10 @@
+package by.it_academy.jd2.service.api;
+
+import by.it_academy.jd2.core.dto.CoordinatesDTO;
+import by.it_academy.jd2.core.dto.ProjectCreateDTO;
+import by.it_academy.jd2.dao.entity.ProjectEntity;
+
+public interface IProjectService extends ICRUDService <ProjectEntity, ProjectCreateDTO, CoordinatesDTO>, IFilterService <ProjectEntity>{
+
+    boolean validateCoordinatesParam(String id);
+}
