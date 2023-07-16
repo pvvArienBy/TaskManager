0a819d8d6be0daaad2a69b7b45ae06a826ac5ed8
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-16 19:34:17 +0300

    ref add addFormatters convert class
    urls control, switch Long to UUID

--- user-service/src/main/java/by/it_academy/jd2/controllers/UserController.java
+++ user-service/src/main/java/by/it_academy/jd2/controllers/UserController.java
@@ -4,11 +4,13 @@ import by.it_academy.jd2.core.dto.UserCreateDTO;
 import by.it_academy.jd2.core.dto.UserDTO;
 import by.it_academy.jd2.dao.entity.UserEntity;
 import by.it_academy.jd2.service.api.IUserService;
-import by.it_academy.jd2.service.util.UserConvertUtil;
+import by.it_academy.jd2.service.convert.UserEntityToDtoConverter;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,37 +20,45 @@ public class UserController {
 
     private final IUserService userService;
 
-    public UserController(IUserService userService) {
+    private final ConversionService conversionService;
+
+    public UserController(IUserService userService, ConversionService conversionService) {
         this.userService = userService;
+        this.conversionService = conversionService;
     }
 
     @GetMapping("/{uuid}")
     public UserDTO findById(@PathVariable UUID uuid) {
-        return UserConvertUtil.toDTO(userService.findById(uuid));
+        return conversionService.convert(userService.findById(uuid), UserDTO.class);
     }
 
     @GetMapping("/")
     public List<UserDTO> findAll() {
-        return UserConvertUtil.toDTO(userService.findAll());
+        List<UserDTO> dtoList = new ArrayList<>();
+        for (UserEntity userEntity : userService.findAll()) {
+           dtoList.add(conversionService.convert(userEntity, UserDTO.class));
+        }
+
+        return dtoList;
     }
 
     @PostMapping("/")
     public ResponseEntity<UserDTO> save(@RequestBody UserCreateDTO userCreateDTO) {
         UserEntity userEntity = userService.save(userCreateDTO);
-        UserDTO userDTO = UserConvertUtil.toDTO(userEntity);
+        UserDTO userDTO = conversionService.convert(userEntity, UserDTO.class);
         return ResponseEntity.status(HttpStatus.CREATED).body(userDTO);
     }
 
-    @PutMapping("/")
-    public ResponseEntity<UserDTO> save(@RequestBody UserCreateDTO userCreateDTO, @RequestParam UUID uuid, @RequestParam Long version) {
-        UserEntity userEntity = userService.save(uuid, version, userCreateDTO);
-        UserDTO userDTO = UserConvertUtil.toDTO(userEntity);
+    @PutMapping("/{uuid}/dt_update/{dt_update}")
+    public ResponseEntity<UserDTO> save(@PathVariable UUID uuid, @PathVariable  Long dt_update, @RequestBody UserCreateDTO userCreateDTO) {
+        UserEntity userEntity = userService.save(uuid, dt_update, userCreateDTO);
+        UserDTO userDTO = conversionService.convert(userEntity, UserDTO.class);
         return ResponseEntity.status(HttpStatus.CREATED).body(userDTO);
     }
 
-    @DeleteMapping("/")
-    public ResponseEntity<Void> delete(@RequestParam UUID uuid, @RequestParam Long version) {
-        userService.delete(uuid, version);
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
+//    @DeleteMapping("/")
+//    public ResponseEntity<Void> delete(@RequestParam UUID uuid, @RequestParam Long version) {
+//        userService.delete(uuid, version);
+//        return ResponseEntity.status(HttpStatus.OK).build();
+//    }
 }
