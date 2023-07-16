77a71efacf9b6f5a78adce91be821442f5970eec
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-16 22:06:38 +0300

    ref  IUserService.java
    add method for registration

--- user-service/src/main/java/by/it_academy/jd2/service/api/IUserService.java
+++ user-service/src/main/java/by/it_academy/jd2/service/api/IUserService.java
@@ -1,8 +1,10 @@
 package by.it_academy.jd2.service.api;
 
 import by.it_academy.jd2.core.dto.UserCreateDTO;
+import by.it_academy.jd2.core.dto.UserRegistrationDTO;
 import by.it_academy.jd2.dao.entity.UserEntity;
 
 public interface IUserService extends ICRUDService <UserEntity, UserCreateDTO>{
 
+    UserEntity save(UserRegistrationDTO item);
 }
