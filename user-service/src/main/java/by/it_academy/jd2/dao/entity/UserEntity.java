72edcb00ac6944ded732efe19ce8b57f77f350ed
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-21 22:09:01 +0300

    ref UserEntity.java add implements UserDetails interface

--- user-service/src/main/java/by/it_academy/jd2/dao/entity/UserEntity.java
+++ user-service/src/main/java/by/it_academy/jd2/dao/entity/UserEntity.java
@@ -5,40 +5,36 @@ import by.it_academy.jd2.core.enums.EStatusUser;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
 @Entity
 @Table(name = "users")
-public class UserEntity implements Serializable {
-
-    static final long serialVersionUID = 7L;
-
+public class UserEntity implements Serializable, UserDetails {
+    static final long serialVersionUID = 8L;
     @Id
     private UUID uuid;
-
     @CreationTimestamp
     @Column(name = "date_create")
     private LocalDateTime dtCreate;
-
     @Version
     @UpdateTimestamp
     @Column(name = "date_update")
     private LocalDateTime dtUpdate;
-
     private String mail;
-
     private String fio;
-
     @Enumerated(EnumType.STRING)
     private ERole role;
-
     @Enumerated(EnumType.STRING)
     private EStatusUser status;
-
     private String password;
 
     public UserEntity() {
@@ -53,6 +49,36 @@ public class UserEntity implements Serializable {
         this.role = role;
         this.status = status;
         this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return mail;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public UUID getUuid() {
