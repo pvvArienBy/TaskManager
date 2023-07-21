72b280cf0ceec9e60c338b06fc2f9ad9688e2868
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-21 22:22:08 +0300

    ref ENotificationDelivery.java     ERole.java     EStatusUser.java

--- user-service/src/main/java/by/it_academy/jd2/core/enums/ENotificationDelivery.java
+++ user-service/src/main/java/by/it_academy/jd2/core/enums/ENotificationDelivery.java
@@ -1,28 +1,6 @@
 package by.it_academy.jd2.core.enums;
 
 public enum ENotificationDelivery {
-
-    MAIL ("mail"),
-
-    TELEGRAM("tg");
-
-    private final String value;
-
-    ENotificationDelivery(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    //todo check
-    public static ENotificationDelivery fromValue(String value) {
-        for (ENotificationDelivery role : ENotificationDelivery.values()) {
-            if (role.value.equalsIgnoreCase(value)) {
-                return role;
-            }
-        }
-        throw new IllegalArgumentException("Invalid ENotificationDelivery value: " + value);
-    }
+    MAIL,
+    TELEGRAM
 }
--- user-service/src/main/java/by/it_academy/jd2/core/enums/ERole.java
+++ user-service/src/main/java/by/it_academy/jd2/core/enums/ERole.java
@@ -1,28 +1,6 @@
 package by.it_academy.jd2.core.enums;
 
 public enum ERole {
-
-    ADMIN("admin"),
-
-    USER("user");
-
-    private final String value;
-
-    ERole(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    //todo check
-    public static ERole fromValue(String value) {
-        for (ERole role : ERole.values()) {
-            if (role.value.equalsIgnoreCase(value)) {
-                return role;
-            }
-        }
-        throw new IllegalArgumentException("Invalid ERole value: " + value);
-    }
+    ADMIN,
+    USER
 }
