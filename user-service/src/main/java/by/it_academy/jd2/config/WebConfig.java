c6bc80ad5b710f99dfe3ed28dc152fe76eb0aa12
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-21 22:33:45 +0300

    ref WebConfig.java add WebSecurityConfig.java

--- user-service/src/main/java/by/it_academy/jd2/config/WebConfig.java
+++ user-service/src/main/java/by/it_academy/jd2/config/WebConfig.java
@@ -1,17 +1,18 @@
 package by.it_academy.jd2.config;
 
-import by.it_academy.jd2.service.convert.*;
+import by.it_academy.jd2.service.convert.StringToLocalDateTimeConverter;
+import by.it_academy.jd2.service.convert.UserCreateDtoToEntityConverter;
+import by.it_academy.jd2.service.convert.UserEntityToDtoConverter;
+import by.it_academy.jd2.service.convert.UserRegistrationDtoToEntityConverter;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,9 +26,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public Jackson2ObjectMapperBuilder jacksonBuilder() {
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-        builder.propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE );
+
+        builder.propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
         builder.modules(new ParameterNamesModule());
         builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
         return builder;
     }
 
