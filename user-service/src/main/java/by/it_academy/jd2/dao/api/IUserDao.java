fde1276dc016f37e2d08bc7b726bf2e79cd2b365
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-21 22:05:08 +0300

    ref IUserDao.java
    ListRepository switch to JpaRepository
    add method findByMail

--- user-service/src/main/java/by/it_academy/jd2/dao/api/IUserDao.java
+++ user-service/src/main/java/by/it_academy/jd2/dao/api/IUserDao.java
@@ -1,7 +1,7 @@
 package by.it_academy.jd2.dao.api;
 
 import by.it_academy.jd2.dao.entity.UserEntity;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,15 +9,12 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface IUserDao extends ListCrudRepository<UserEntity, UUID> {
-
+public interface IUserDao extends JpaRepository<UserEntity, UUID> {
     @Override
     List<UserEntity> findAll();
-
     @Override
     <S extends UserEntity> S save(S entity);
-
     @Override
     Optional<UserEntity> findById(UUID id);
-
+    Optional<UserEntity> findByMail(String mail);
 }
