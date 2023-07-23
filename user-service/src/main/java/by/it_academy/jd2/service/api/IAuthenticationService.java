a2842afcde8e1177ed11137a024bff338bda0646
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-23 20:27:18 +0300

    ref ICRUDService.java (cstyle) and ref IAuthenticationService.java
    add method register() and confirmToken()

--- user-service/src/main/java/by/it_academy/jd2/service/api/IAuthenticationService.java
+++ user-service/src/main/java/by/it_academy/jd2/service/api/IAuthenticationService.java
@@ -3,10 +3,11 @@ package by.it_academy.jd2.service.api;
 import by.it_academy.jd2.core.dto.TokenDTO;
 import by.it_academy.jd2.core.dto.UserLoginDTO;
 import by.it_academy.jd2.core.dto.UserRegistrationDTO;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface IAuthenticationService {
-
+public interface IAuthenticationService extends UserDetailsService {
     TokenDTO registration(UserRegistrationDTO dto);
-
     TokenDTO authentication(UserLoginDTO dto);
+    String register(UserRegistrationDTO dto);
+    String confirmToken(String token);
 }
--- user-service/src/main/java/by/it_academy/jd2/service/api/ICRUDService.java
+++ user-service/src/main/java/by/it_academy/jd2/service/api/ICRUDService.java
@@ -5,16 +5,11 @@ import java.util.List;
 import java.util.UUID;
 
 public interface ICRUDService<T, S> {
-
     List<T> findAll();
-
     T findById(UUID uuid);
-
     T save(S item);
-
     //todo ref
 //    void delete(UUID uuid, Long version);
-
     T save(UUID uuid, LocalDateTime version, S item);
 }
 
