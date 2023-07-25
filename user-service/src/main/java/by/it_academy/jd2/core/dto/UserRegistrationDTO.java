dba721e2640ffd4b20a85602bd74f766a70be1c0
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-25 19:40:22 +0300

    ref UserRegistrationDTO.java

--- user-service/src/main/java/by/it_academy/jd2/core/dto/UserRegistrationDTO.java
+++ user-service/src/main/java/by/it_academy/jd2/core/dto/UserRegistrationDTO.java
@@ -1,8 +1,20 @@
 package by.it_academy.jd2.core.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class UserRegistrationDTO {
-    private String mail;
+    @NotEmpty
+    @Size(max = 50)
+    @Pattern(regexp = "^[^\\d]+$", message = "ФИО не должно содержать цифр")
     private String fio;
+    @NotEmpty
+    @Email(message = "Некорректный адрес электронной почты")
+    private String mail;
+    @NotEmpty
+    @Size(min = 6, max = 30, message = "Пароль должен содержать от {min} до {max} символов")
     private String password;
 
     public UserRegistrationDTO() {
