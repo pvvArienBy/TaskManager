bb9486fbd96334497601302449b7f7bbbd3175a9
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-27 12:35:08 +0300

    ref  UserCreateUpdateDTO.java
    rename class
    and add validation

--- user-service/src/main/java/by/it_academy/jd2/core/dto/UserCreateUpdateDTO.java
+++ user-service/src/main/java/by/it_academy/jd2/core/dto/UserCreateUpdateDTO.java
@@ -2,15 +2,29 @@ package by.it_academy.jd2.core.dto;
 
 import by.it_academy.jd2.core.enums.ERole;
 import by.it_academy.jd2.core.enums.EStatusUser;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.*;
 
-public class UserCreateDTO {
+public class UserCreateUpdateDTO {
+    @NotBlank(message = "mail не должен быть пустым")
+    @Email(message = "Некорректный адрес электронной почты")
     private String mail;
+    @NotBlank(message = "fio не должен быть пустым")
+    @Size(max = 255)
+    @Pattern(regexp = "^[^\\d]+$", message = "fio не должно содержать цифр")
     private String fio;
+    @NotNull(message = "role не должен быть пустым")
+    @Enumerated(EnumType.STRING)
     private ERole role;
+    @NotNull(message = "status не должен быть пустым")
+    @Enumerated(EnumType.STRING)
     private EStatusUser status;
+    @NotBlank(message = "password не должен быть пустым")
+    @Size(min = 6, max = 30, message = "Пароль должен содержать от {min} до {max} символов")
     private String password;
 
-    public UserCreateDTO() {
+    public UserCreateUpdateDTO() {
     }
 
     public String getMail() {
