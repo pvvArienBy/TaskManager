4efb1a1586efd54e982120fa87cacf690da54337
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-31 18:14:53 +0300

    ref UserServiceApplication.java
    add @EnableFeignClients

--- user-service/src/main/java/by/it_academy/jd2/UserServiceApplication.java
+++ user-service/src/main/java/by/it_academy/jd2/UserServiceApplication.java
@@ -2,16 +2,17 @@ package by.it_academy.jd2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableFeignClients
 @EnableJpaRepositories
 @EnableTransactionManagement
 @SpringBootApplication
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 }
