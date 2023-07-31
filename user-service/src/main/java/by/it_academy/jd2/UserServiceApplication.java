d1a94735edcaa4aa1ecd261d4bf01531f8853efc
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-01 00:28:17 +0300

    add UserServiceApplication.java
    @EnableDiscoveryClient +

--- user-service/src/main/java/by/it_academy/jd2/UserServiceApplication.java
+++ user-service/src/main/java/by/it_academy/jd2/UserServiceApplication.java
@@ -2,11 +2,12 @@ package by.it_academy.jd2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@E
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableJpaRepositories
 @EnableTransactionManagement
