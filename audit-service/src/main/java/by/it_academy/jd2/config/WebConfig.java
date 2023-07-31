cc7db1adf29a2068189feb0e2f7280908cfd4001
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-07-31 10:32:23 +0300

    add  WebConfig.java to audit-service add  UserDTO.java  to audit-service add PageDTO.java to audit-service

---
+++ audit-service/src/main/java/by/it_academy/jd2/config/WebConfig.java
@@ -0,0 +1,60 @@
+package by.it_academy.jd2.config;
+
+import by.it_academy.jd2.core.errors.ErrorResponse;
+import by.it_academy.jd2.core.errors.StructuredErrorResponse;
+import by.it_academy.jd2.service.convert.*;
+import by.it_academy.jd2.service.util.ErrorResponseJsonComponent;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public Jackson2ObjectMapperBuilder jacksonBuilder() {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+
+        builder.propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        builder.modules(new ParameterNamesModule());
+        builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        return builder;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json()
+                .modules(new ParameterNamesModule())
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .build();
+
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(StructuredErrorResponse.class, new ErrorResponseJsonComponent.StructuredErrorResponseSerializer());
+        module.addSerializer(ErrorResponse.class, new ErrorResponseJsonComponent.ErrorResponseSerializer());
+        objectMapper.registerModule(module);
+
+        converters.add(new MappingJackson2HttpMessageConverter(objectMapper));
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new AuditCreateDtoToEntityConverter());
+        registry.addConverter(new AuditEntityToDtoConverter());
+        registry.addConverter(new StringToLocalDateTimeConverter());
+        registry.addConverter(new PageEntityToPageDtoConverter(new AuditEntityToDtoConverter()));
+    }
+}
