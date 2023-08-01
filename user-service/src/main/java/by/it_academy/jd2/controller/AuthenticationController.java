17e263600b2d7a093685eb9276d4a6c59d0f4c56
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-01 13:12:39 +0300

    ref AuthenticationController.java add test method (feign)

--- user-service/src/main/java/by/it_academy/jd2/controller/AuthenticationController.java
+++ user-service/src/main/java/by/it_academy/jd2/controller/AuthenticationController.java
@@ -1,23 +1,24 @@
 package by.it_academy.jd2.controller;
 
-import by.it_academy.jd2.core.dto.TokenDTO;
-import by.it_academy.jd2.core.dto.UserLoginDTO;
-import by.it_academy.jd2.core.dto.UserRegistrationDTO;
+import by.it_academy.jd2.core.dto.*;
+import by.it_academy.jd2.service.api.IAuditService;
 import by.it_academy.jd2.service.api.IAuthenticationService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
 public class AuthenticationController {
     private final IAuthenticationService authService;
+    private final IAuditService auditService;
 
-    public AuthenticationController(IAuthenticationService userAuthService) {
+    public AuthenticationController(IAuthenticationService userAuthService, IAuditService auditService) {
         this.authService = userAuthService;
+        this.auditService = auditService;
     }
 
     @PostMapping("/registration")
@@ -28,5 +29,11 @@ public class AuthenticationController {
     @PostMapping("/login")
     public ResponseEntity<TokenDTO> authentication(@RequestBody @Valid UserLoginDTO dto) {
         return ResponseEntity.ok(this.authService.authentication(dto));
+    }
+
+    @GetMapping("/test/{uuid}")
+    public ResponseEntity<AuditDTO> findByIdAudit(@PathVariable UUID uuid) {
+        AuditDTO userDTO = this.auditService.test(uuid);
+        return ResponseEntity.status(HttpStatus.OK).body(userDTO);
     }
 }
