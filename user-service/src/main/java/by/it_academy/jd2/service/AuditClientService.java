905710c54ea0835012014ce21f3f0edfdc116b83
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-01 13:10:51 +0300

    ref AuditClientService.java impl interface

--- user-service/src/main/java/by/it_academy/jd2/service/AuditClientService.java
+++ user-service/src/main/java/by/it_academy/jd2/service/AuditClientService.java
@@ -1,4 +1,26 @@
 package by.it_academy.jd2.service;
 
-public class AuditClientService {
+import by.it_academy.jd2.core.dto.AuditDTO;
+import by.it_academy.jd2.openfeignclient.TestAuditClient;
+import by.it_academy.jd2.service.api.IAuditService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class AuditClientService implements IAuditService {
+
+    private final TestAuditClient auditClient;
+
+    public AuditClientService(TestAuditClient auditClient) {
+        this.auditClient = auditClient;
+    }
+
+    @Override
+    public AuditDTO test(UUID id) {
+        ResponseEntity<AuditDTO> auditResponseEntity = this.auditClient.findById(id);
+        AuditDTO dto = auditResponseEntity.getBody();
+        return dto;
+    }
 }
