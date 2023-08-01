0ee88d87d19b64add413aabfa91e18185694c11c
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-01 13:10:20 +0300

    add IAuditService.java and test method

---
+++ user-service/src/main/java/by/it_academy/jd2/service/api/IAuditService.java
@@ -0,0 +1,10 @@
+package by.it_academy.jd2.service.api;
+
+import by.it_academy.jd2.core.dto.AuditDTO;
+
+import java.util.UUID;
+
+public interface IAuditService {
+
+    AuditDTO test (UUID id);
+}
