5c5d4fb4b132205f84543ac46fc3fefe94b4eba9
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-02 14:00:29 +0300

    ref UserServiceApplication.java
    фвв EnableConfigurationProperties({JWTProperty.class})

--- user-service/src/main/java/by/it_academy/jd2/UserServiceApplication.java
+++ user-service/src/main/java/by/it_academy/jd2/UserServiceApplication.java
@@ -1,7 +1,9 @@
 package by.it_academy.jd2;
 
+import by.it_academy.jd2.config.properties.JWTProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableFeignClients
 @EnableJpaRepositories
 @EnableTransactionManagement
+@EnableConfigurationProperties({JWTProperty.class})
 @SpringBootApplication
 public class UserServiceApplication {
 
