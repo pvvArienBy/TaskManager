2843d6e5944ea3294d05e3f585d7223e2ea1ea20
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-02 13:14:42 +0300

    ref AuditEntityToDtoConverter.java.java
    UserDTO switch  to UserCheckDTO

--- audit-service/src/main/java/by/it_academy/jd2/service/convert/AuditEntityToDtoConverter.java
+++ audit-service/src/main/java/by/it_academy/jd2/service/convert/AuditEntityToDtoConverter.java
@@ -1,7 +1,7 @@
 package by.it_academy.jd2.service.convert;
 
 import by.it_academy.jd2.core.dto.AuditDTO;
-import by.it_academy.jd2.core.dto.UserDTO;
+import by.it_academy.jd2.core.dto.UserCheckDTO;
 import by.it_academy.jd2.dao.entity.AuditEntity;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
@@ -13,13 +13,12 @@ public class AuditEntityToDtoConverter implements Converter<AuditEntity, AuditDT
     @Override
     public AuditDTO convert(AuditEntity item) {
         AuditDTO dto = new AuditDTO();
-        UserDTO userDTO = new UserDTO();
+        UserCheckDTO userDTO = new UserCheckDTO();
 
         userDTO.setUuid(item.getUserUuid());
         userDTO.setMail(item.getUserMail());
         userDTO.setFio(item.getUserFio());
         userDTO.setRole(item.getUserRole());
-
         dto.setUuid(item.getUuid());
         dto.setDtCreate(item.getDtCreate().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
         dto.setUser(userDTO);
