7e6c3db428d2b67f16d85107f5bf6b0d237b5b89
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-03 12:40:48 +0300

    ref AuthenticationController.java rename method me to details and new realization

--- user-service/src/main/java/by/it_academy/jd2/controller/AuthenticationController.java
+++ user-service/src/main/java/by/it_academy/jd2/controller/AuthenticationController.java
@@ -1,25 +1,36 @@
 package by.it_academy.jd2.controller;
 
-import by.it_academy.jd2.core.dto.TokenDTO;
-import by.it_academy.jd2.core.dto.UserCheckDTO;
-import by.it_academy.jd2.core.dto.UserLoginDTO;
-import by.it_academy.jd2.core.dto.UserRegistrationDTO;
+import by.it_academy.jd2.core.dto.*;
+import by.it_academy.jd2.dao.entity.UserEntity;
+import by.it_academy.jd2.service.UserHolder;
 import by.it_academy.jd2.service.api.IAuditService;
 import by.it_academy.jd2.service.api.IAuthenticationService;
+import by.it_academy.jd2.service.api.IUserService;
+import by.it_academy.jd2.service.exceptions.EntityNotFoundException;
 import jakarta.validation.Valid;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class AuthenticationController {
     private final IAuthenticationService authService;
-    private final IAuditService auditService;
+    private final IUserService userService;
+    private final ConversionService conversionService;
+    private final UserHolder userHolder;
 
-    public AuthenticationController(IAuthenticationService authService, IAuditService auditService) {
+    public AuthenticationController(IAuthenticationService authService,
+                                    IUserService userService,
+                                    ConversionService conversionService,
+                                    UserHolder userHolder) {
         this.authService = authService;
-        this.auditService = auditService;
+        this.userService = userService;
+        this.conversionService = conversionService;
+        this.userHolder = userHolder;
     }
 
     @PostMapping("/registration")
@@ -33,9 +44,16 @@ public class AuthenticationController {
     }
 
     @GetMapping("/me")
-    public ResponseEntity<UserCheckDTO> me(@RequestHeader("Authorization") String authorizationHeader) {
-        UserCheckDTO dto = this.auditService.meDetails(authorizationHeader);
-        return ResponseEntity.status(HttpStatus.OK).body(dto);
+    public ResponseEntity<UserDTO> details() {
+
+        String username  =this.userHolder.getUser()
+                .getUsername();
+        Optional<UserEntity> item = this.userService
+                .findByMail(username);
+        UserEntity entity = item.orElseThrow(() -> new EntityNotFoundException(
+                "Ошибка данных из контекста, попробуйте еще раз после новой авторизации пользователя!"));
+
+        return ResponseEntity.status(HttpStatus.OK).body(conversionService.convert(entity, UserDTO.class));
     }
 
     @GetMapping("/registration/confirm")
