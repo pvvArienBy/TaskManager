5656a1855910429068d7b9d2d33f9a85ab2243a5
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-03 20:33:58 +0300

    ref IMailSenderService.java remove validation()

--- user-service/src/main/java/by/it_academy/jd2/service/api/IMailSenderService.java
+++ user-service/src/main/java/by/it_academy/jd2/service/api/IMailSenderService.java
@@ -4,5 +4,4 @@ import by.it_academy.jd2.core.dto.UserRegistrationDTO;
 
 public interface IMailSenderService {
     void send(UserRegistrationDTO dto, String link);
-    boolean validation(String s);
 }
