a5c22a1de4c22af47e6cdb24fc4f66280b52c102
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-03 23:32:47 +0300

    ref  MyUserDetailsService.java ref loadUserByUsername()

--- user-service/src/main/java/by/it_academy/jd2/service/MyUserDetailsService.java
+++ user-service/src/main/java/by/it_academy/jd2/service/MyUserDetailsService.java
@@ -21,6 +21,6 @@ public class MyUserDetailsService implements IMyUserDetailService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<UserEntity> userOptional = this.userDao.findByMail(username);
-        return userOptional.orElseThrow(() -> new EntityNotFoundException("Объект не найден!"));
+        return userOptional.orElseThrow(() -> new EntityNotFoundException("User is not found!"));
     }
 }
