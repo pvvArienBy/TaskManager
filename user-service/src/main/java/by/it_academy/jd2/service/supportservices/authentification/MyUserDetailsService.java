b2c4e23c1a0a5c1482e368b51e52f6b4d9900ff3
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-07 22:45:24 +0300

    ref MyUserDetailsService.java IUserDao switch to userService

--- user-service/src/main/java/by/it_academy/jd2/service/supportservices/authentification/MyUserDetailsService.java
+++ user-service/src/main/java/by/it_academy/jd2/service/supportservices/authentification/MyUserDetailsService.java
@@ -1,27 +1,27 @@
 package by.it_academy.jd2.service.supportservices.authentification;
 
-import by.it_academy.jd2.dao.repositories.IUserDao;
-import by.it_academy.jd2.dao.entity.UserEntity;
-import by.it_academy.jd2.service.api.IMyUserDetailService;
 import by.it_academy.jd2.core.exceptions.EntityNotFoundException;
+import by.it_academy.jd2.dao.entity.UserEntity;
+import by.it_academy.jd2.service.api.IUserService;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Service
-public class MyUserDetailsService implements IMyUserDetailService {
-    private static final String USER_NOT_FOUND = "User is not found";
-    private final IUserDao userDao;
 
-    public MyUserDetailsService(IUserDao userDao) {
-        this.userDao = userDao;
+public class MyUserDetailsService implements UserDetailsService {
+    private static final String USER_NOT_FOUND = "User is not found";
+
+    private final IUserService userService;
+
+    public MyUserDetailsService(IUserService userService) {
+        this.userService = userService;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<UserEntity> userOptional = this.userDao.findByMail(username);
+        Optional<UserEntity> userOptional = this.userService.findByMail(username);
         return userOptional.orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND));
     }
 }
