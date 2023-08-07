a330b852799957c5e12ce9d6f0a727644fe0e70f
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-07 22:40:41 +0300

    rem IMyUserDetailService.java

--- user-service/src/main/java/by/it_academy/jd2/service/api/IMyUserDetailService.java
+++
@@ -1,6 +0,0 @@
-package by.it_academy.jd2.service.api;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface IMyUserDetailService extends UserDetailsService {
-}
