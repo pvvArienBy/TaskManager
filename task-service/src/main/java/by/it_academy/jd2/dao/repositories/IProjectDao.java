aa64a3c67db80da5db0627dba393d8c7bf565d40
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-11 19:40:07 +0300

    ref IProjectDao.java add method findByStatusNotLike(), existsByUuidAndStaffContaining(), existsByManager()

--- task-service/src/main/java/by/it_academy/jd2/dao/repositories/IProjectDao.java
+++ task-service/src/main/java/by/it_academy/jd2/dao/repositories/IProjectDao.java
@@ -1,17 +1,21 @@
 package by.it_academy.jd2.dao.repositories;
 
+import by.it_academy.jd2.core.enums.EProjectStatus;
 import by.it_academy.jd2.dao.entity.ProjectEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface IProjectDao extends JpaRepository<ProjectEntity, UUID> {
-    @Override
-    <S extends ProjectEntity> S save(S entity);
-    //    Optional<ProjectEntity> findByMail(String mail);
-//
-//    boolean existsByMail(String mail); // TODO: 09.08.2023  
+
+    Page<ProjectEntity> findByStatusNotLike(EProjectStatus status, Pageable pageable);
+
+    boolean existsByUuidAndStaffContaining(UUID projectUuid, UUID staffUuid);
+
+    boolean existsByManager(UUID managerUuid);
+
 }
