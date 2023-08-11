5edd20894eb607533d4f1655bf6639ea90dd0dfb
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-11 19:41:59 +0300

    ref IUserClientService.java add method checkForProject()

--- task-service/src/main/java/by/it_academy/jd2/service/api/feign/IUserClientService.java
+++ task-service/src/main/java/by/it_academy/jd2/service/api/feign/IUserClientService.java
@@ -1,13 +1,20 @@
 package by.it_academy.jd2.service.api.feign;
 
+import by.it_academy.jd2.core.dto.ResultUsersVerificationDTO;
+import by.it_academy.jd2.core.dto.UsersVerificationDTO;
 import org.example.mylib.tm.itacademy.dto.UserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "USERS", path = "/users")
+@FeignClient(name = "USERS")
 public interface IUserClientService {
-    @GetMapping("/me")
+    @GetMapping("/users/me")
     ResponseEntity<UserDTO> meDetails(@RequestHeader("Authorization") String authorizationHeader);
+
+    @PostMapping("/internal")
+    ResponseEntity<ResultUsersVerificationDTO> checkForProject(@RequestBody UsersVerificationDTO dto);
 }
