8d4a64c2a03c1ae4c2e4dcee2a061afd988191a6
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-11 19:43:03 +0300

    ref JwtService.java add method extractUuid()

--- task-service/src/main/java/by/it_academy/jd2/service/supportservices/JwtService.java
+++ task-service/src/main/java/by/it_academy/jd2/service/supportservices/JwtService.java
@@ -30,6 +30,11 @@ public class JwtService {
         return username;
     }
 
+    public String extractUuid(String token) {
+        String uuid = extractClaim(token, claims -> claims.get("uuid", String.class));
+        return uuid;
+    }
+
     public <T> T extractClaim(String token,
                               Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
