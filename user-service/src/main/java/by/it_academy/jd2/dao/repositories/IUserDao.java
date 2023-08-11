05e7a636e403a885a60938039ad4bd896895061c
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-11 19:42:38 +0300

    ref IUserDao.java add method countByUuidIn() & existsByUuidAndRole()

--- user-service/src/main/java/by/it_academy/jd2/dao/repositories/IUserDao.java
+++ user-service/src/main/java/by/it_academy/jd2/dao/repositories/IUserDao.java
@@ -1,9 +1,11 @@
 package by.it_academy.jd2.dao.repositories;
 
 import by.it_academy.jd2.dao.entity.UserEntity;
+import org.example.mylib.tm.itacademy.enums.ERole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,4 +15,9 @@ public interface IUserDao extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByMail(String mail);
 
     boolean existsByMail(String mail);
+
+    long countByUuidIn(List<UUID> uuidList);
+
+    boolean existsByUuidAndRole(UUID uuid, ERole role);
+
 }
