ee44822d942228d517266aa491632bbc44f6f73d
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-12 14:55:47 +0300

    ref UserHolder.java
    add IUserClientService & ConversionService
    add method checkAdminRole()

--- task-service/src/main/java/by/it_academy/jd2/service/supportservices/UserHolder.java
+++ task-service/src/main/java/by/it_academy/jd2/service/supportservices/UserHolder.java
@@ -1,6 +1,10 @@
 package by.it_academy.jd2.service.supportservices;
 
+import by.it_academy.jd2.service.api.feign.IUserClientService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.example.mylib.tm.itacademy.dto.UserCheckDTO;
+import org.example.mylib.tm.itacademy.enums.ERole;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -11,6 +15,13 @@ import java.util.Objects;
 
 @Component
 public class UserHolder {
+    private final IUserClientService userClientService;
+    private final ConversionService conversionService;
+
+    public UserHolder(IUserClientService userClientService, ConversionService conversionService) {
+        this.userClientService = userClientService;
+        this.conversionService = conversionService;
+    }
 
     public UserDetails getUser() {
         return (UserDetails) SecurityContextHolder
@@ -22,5 +33,13 @@ public class UserHolder {
                 (ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder
                         .getRequestAttributes())).getRequest();
         return request.getHeader("Authorization");
+    }
+
+    public boolean checkAdminRole() {
+        UserCheckDTO userCheckDTO = this.conversionService.convert(
+                this.userClientService.meDetails(
+                        getToken()).getBody(), UserCheckDTO.class);
+
+        return userCheckDTO.getRole().equals(ERole.ADMIN);
     }
 }
