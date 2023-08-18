459fa5d44a689cc438cebf0654247858dd57b00e
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-18 12:57:05 +0300

    ref IReportService.java
    add method getUrlReport() & getFileName()

--- report-service/src/main/java/by/it_academy/jd2/service/api/IReportService.java
+++ report-service/src/main/java/by/it_academy/jd2/service/api/IReportService.java
@@ -4,6 +4,7 @@ import by.it_academy.jd2.core.dto.ReportCreateDTO;
 import by.it_academy.jd2.dao.entity.ReportEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.UUID;
 
@@ -14,4 +15,8 @@ public interface IReportService {
     ReportEntity get(UUID uuid);
 
     ReportEntity save(ReportCreateDTO item);
+
+    RedirectView getUrlReport(UUID uuid);
+
+    String getFileName(UUID uuid);
 }
