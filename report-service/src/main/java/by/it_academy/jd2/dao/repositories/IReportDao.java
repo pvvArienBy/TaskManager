d56a4c244087c7cebae7377572f0eb0cc3116ad8
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-08-18 12:55:09 +0300

    ref IReportDao.java clean comnents

--- report-service/src/main/java/by/it_academy/jd2/dao/repositories/IReportDao.java
+++ report-service/src/main/java/by/it_academy/jd2/dao/repositories/IReportDao.java
@@ -1,23 +1,13 @@
 package by.it_academy.jd2.dao.repositories;
 
 import by.it_academy.jd2.dao.entity.ReportEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface IReportDao extends JpaRepository<ReportEntity, UUID> {
-
-    // TODO: 17.08.2023
-//    @Override
-//    <S extends ReportEntity> S save(S entity);
-//
-//    @Override
-//    Optional<ReportEntity> findById(UUID id);
-//
-//    @Override
-//    Page<ReportEntity> findAll(Pageable pageable);
+    ReportEntity findFileNameByUuid(UUID uuid);
 }
