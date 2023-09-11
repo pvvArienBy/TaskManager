001ff3e6abb96874e7a1ed66abdc0b43294a6d45
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-09-11 11:36:58 +0300

    ref ReportServiceApplication.java
    add @EnableScheduling

--- report-service/src/main/java/by/it_academy/jd2/ReportServiceApplication.java
+++ report-service/src/main/java/by/it_academy/jd2/ReportServiceApplication.java
@@ -8,8 +8,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableScheduling
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableJpaRepositories
