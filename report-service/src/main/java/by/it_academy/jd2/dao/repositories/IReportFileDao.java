c122401b220ff2f2db054d595dd7ea389b60b674
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-09-11 11:41:24 +0300

    ref findByReportUuid
    Optional add in findByReportUuid()

--- report-service/src/main/java/by/it_academy/jd2/dao/repositories/IReportFileDao.java
+++ report-service/src/main/java/by/it_academy/jd2/dao/repositories/IReportFileDao.java
@@ -4,10 +4,11 @@ import by.it_academy.jd2.dao.entity.ReportFileEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface IReportFileDao extends JpaRepository<ReportFileEntity, Long> {
-    ReportFileEntity findByReportUuid(UUID uuid);
+    Optional<ReportFileEntity>  findByReportUuid(UUID uuid);
     boolean existsByReportUuid(UUID uuid);
 }
