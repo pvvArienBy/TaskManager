351c36837d0993072f12aa718aa1d8efa17fe6f3
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-09-13 18:46:52 +0300

    ref AuditCreateDtoToEntityConverter.java
    cosm

--- audit-service/src/main/java/by/it_academy/jd2/service/converters/AuditCreateDtoToEntityConverter.java
+++ audit-service/src/main/java/by/it_academy/jd2/service/converters/AuditCreateDtoToEntityConverter.java
@@ -1,6 +1,6 @@
 package by.it_academy.jd2.service.converters;
 
-import by.it_academy.jd2.dao.entity.AuditDoc;
+import by.it_academy.jd2.dao.entity.Audit;
 import org.example.mylib.tm.itacademy.dto.AuditCreateDTO;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
@@ -9,10 +9,10 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Component
-public class AuditCreateDtoToEntityConverter implements Converter<AuditCreateDTO, AuditDoc> {
+public class AuditCreateDtoToEntityConverter implements Converter<AuditCreateDTO, Audit> {
     @Override
-    public AuditDoc convert(AuditCreateDTO dto) {
-        AuditDoc entity = new AuditDoc();
+    public Audit convert(AuditCreateDTO dto) {
+        Audit entity = new Audit();
 
         entity.setUuid(UUID.randomUUID().toString());
         entity.setDtCreate(LocalDateTime.now());
