d3674b2989815fc9dea1957bcc3a608ab85b70e2
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-09-13 21:23:41 +0300

    ref IAuditService.java     PageEntityToPageDtoConverter.java     pom.xml add depend/ mongoDB

--- audit-service/src/main/java/by/it_academy/jd2/service/api/IAuditService.java
+++ audit-service/src/main/java/by/it_academy/jd2/service/api/IAuditService.java
@@ -1,7 +1,6 @@
 package by.it_academy.jd2.service.api;
 
-import by.it_academy.jd2.core.dto.AuditDTO;
-import by.it_academy.jd2.dao.entity.AuditEntity;
+import by.it_academy.jd2.dao.entity.Audit;
 import org.example.mylib.tm.itacademy.dto.AuditCreateDTO;
 import org.example.mylib.tm.itacademy.dto.ParamDTO;
 import org.springframework.data.domain.Page;
@@ -11,11 +10,11 @@ import java.util.List;
 import java.util.UUID;
 
 public interface IAuditService {
-    AuditEntity findById(UUID uuid);
+    Audit findById(UUID uuid);
 
-    AuditEntity save(AuditCreateDTO item);
+    Audit save(AuditCreateDTO item);
 
-    Page<AuditEntity> findAll(PageRequest of);
+    Page<Audit> findAll(PageRequest of);
 
-    List<AuditEntity> getList(ParamDTO dto);
+    List<Audit> getList(ParamDTO dto);
 }
