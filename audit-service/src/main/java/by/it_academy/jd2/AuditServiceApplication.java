2f5440733fc2cc59cf89e92724be68f8e538d7b4
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-09-13 21:20:27 +0300

    ref AuditServiceApplication.java
    remove @EnableJpaRepositories

--- audit-service/src/main/java/by/it_academy/jd2/AuditServiceApplication.java
+++ audit-service/src/main/java/by/it_academy/jd2/AuditServiceApplication.java
@@ -6,18 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableJpaRepositories
 @EnableTransactionManagement
 @EnableConfigurationProperties({JWTProperty.class})
 @SpringBootApplication
 public class AuditServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuditServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuditServiceApplication.class, args);
+    }
 }
